ae9492c5d5f523748a0c07a1034dcf6beab9ba8b
pwrona <przemekwrona25@gmail.com>
2022-01-07 17:15:26 +0100

    feat: configure core and hermes module

---
+++ iot-hermes/src/main/java/pl/wrona/iothermes/IotHermesApplication.java
@@ -0,0 +1,13 @@
+package pl.wrona.iothermes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotHermesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IotHermesApplication.class, args);
+    }
+
+}
