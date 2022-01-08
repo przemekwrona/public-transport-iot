1976eda490a6edb8e10eed5b4a03a190a6e288ec
pwrona <przemekwrona25@gmail.com>
2022-01-08 15:00:17 +0100

    feat: add scheduling and query Warsaw API every 10s

--- iot-hermes/src/main/java/pl/wrona/iothermes/client/WarsawPublicTransportService.java
+++ iot-hermes/src/main/java/pl/wrona/iothermes/client/WarsawPublicTransportService.java
@@ -3,9 +3,14 @@ package pl.wrona.iothermes.client;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import pl.wrona.iothermes.WarsawUmApiConfiguration;
+import pl.wrona.warsaw.transport.api.model.WarsawVehicle;
 import pl.wrona.warsaw.transport.api.model.WarsawVehicles;
+
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Component
@@ -15,12 +20,18 @@ public class WarsawPublicTransportService {
     private final WarsawUmApiConfiguration warsawUmApiConfiguration;
     private final WarsawPublicTransportClient warsawPublicTransportClient;
 
-    public WarsawVehicles getBuses() {
-        return warsawPublicTransportClient.getVehicles(warsawUmApiConfiguration.getResourceId(), warsawUmApiConfiguration.getApikey(), "1").getBody();
+    public List<WarsawVehicle> getBuses() {
+        return Optional.ofNullable(warsawPublicTransportClient.getVehicles(warsawUmApiConfiguration.getResourceId(), warsawUmApiConfiguration.getApikey(), "1"))
+                .map(ResponseEntity::getBody)
+                .map(WarsawVehicles::getResult)
+                .orElse(List.of());
     }
 
-    public WarsawVehicles getTrams() {
-        return warsawPublicTransportClient.getVehicles(warsawUmApiConfiguration.getResourceId(), warsawUmApiConfiguration.getApikey(), "2").getBody();
+    public List<WarsawVehicle> getTrams() {
+        return Optional.ofNullable(warsawPublicTransportClient.getVehicles(warsawUmApiConfiguration.getResourceId(), warsawUmApiConfiguration.getApikey(), "2"))
+                .map(ResponseEntity::getBody)
+                .map(WarsawVehicles::getResult)
+                .orElse(List.of());
     }
 
 }
---
+++ iot-hermes/src/main/java/pl/wrona/iothermes/job/WarsawVehicleJob.java
@@ -0,0 +1,24 @@
+package pl.wrona.iothermes.job;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import pl.wrona.iothermes.service.WarsawService;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class WarsawVehicleJob implements Runnable {
+
+    public static final int TEN_SECONDS = 10 * 1000;
+
+    private final WarsawService warsawService;
+
+    @Override
+    @Scheduled(fixedRate = TEN_SECONDS)
+    public void run() {
+        warsawService.getAndSaveVehicles();
+    }
+
+}
