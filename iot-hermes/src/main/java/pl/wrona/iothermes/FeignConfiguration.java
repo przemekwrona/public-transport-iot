256613b6051175bfab1a5ad7a66b8f36ac1fc1ff
pwrona <przemekwrona25@gmail.com>
2022-01-09 02:20:03 +0100

    feat: aff feign logger level

---
+++ iot-hermes/src/main/java/pl/wrona/iothermes/FeignConfiguration.java
@@ -0,0 +1,15 @@
+package pl.wrona.iothermes;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfiguration {
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.BASIC;
+    }
+
+}
