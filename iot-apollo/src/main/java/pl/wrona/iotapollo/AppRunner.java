c6a226f0f49f4f8cd65f71506235d388bfdc956c
pwrona <przemekwrona25@gmail.com>
2022-02-04 08:46:28 +0100

    feat: add app runner

---
+++ iot-apollo/src/main/java/pl/wrona/iotapollo/AppRunner.java
@@ -0,0 +1,48 @@
+package pl.wrona.iotapollo;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import pl.wrona.iotapollo.client.WarsawApiService;
+import pl.wrona.iotapollo.client.WarsawStopService;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class AppRunner implements CommandLineRunner {
+
+    private WarsawStopService warsawStopService;
+    private WarsawApiService warsawApiService;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+//        log.info("{}", warsawStopService.getClosestStop(52.24f, 21.00f, "9"));
+//        log.info("{}", warsawStopService.getClosestStop(52.24f, 21.00f, "9"));
+//        log.info("{}", warsawStopService.getClosestStop(52.24f, 21.02f, "9"));
+//        log.info("{}", warsawStopService.getClosestStop(52.24f, 21.04f, "9"));
+//        log.info("{}", warsawStopService.getClosestStop(52.24f, 21.08f, "9"));
+
+//        log.info("Get Closest Stops");
+//        warsawStopService.getClosestStop(52.02f, 21.00f);
+//        log.info("Get Closest Stops");
+//        warsawStopService.getClosestStop(32.23f, 23.43f);
+//        log.info("Get Closest Stops");
+//        warsawStopService.getClosestStop(32.23f, 23.43f);
+//        log.info("Get Closest Stops");
+//        warsawStopService.getClosestStop(32.23f, 23.43f);
+
+
+        log.info("Do sthd");
+        warsawStopService.getLinesOnStop("4121", "03");
+        log.info("Do sthd");
+        warsawStopService.getLinesOnStop("4121", "03");
+        log.info("Do sthd");
+        warsawStopService.getLinesOnStop("4121", "03");
+//        warsawStopService.getClosestStop(52.55f, 21.05f);
+//        warsawStopService.getClosestStop(52.55f, 21.05f);
+//        warsawStopService.getClosestStop(52.55f, 21.05f);
+    }
+
+}
