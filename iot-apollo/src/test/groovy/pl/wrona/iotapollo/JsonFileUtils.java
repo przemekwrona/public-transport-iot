099cfe4b7b2e78d19c740adcbcb32e5427d02800
pwrona <przemekwrona25@gmail.com>
2022-02-11 11:31:25 +0100

    feat: add test to get the closest stop

---
+++ iot-apollo/src/test/groovy/pl/wrona/iotapollo/JsonFileUtils.java
@@ -0,0 +1,17 @@
+package pl.wrona.iotapollo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class JsonFileUtils {
+
+    static ObjectMapper objectMapper = new ObjectMapper();
+
+    public static <E> E readJson(String path, Class<E> clazz) throws IOException {
+        return objectMapper.readValue(IOUtils.resourceToString(String.format("/json%s", path), StandardCharsets.UTF_8), clazz);
+    }
+
+}
