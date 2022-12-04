7fee9f52047fa17b1f2b93e0a4a501e07a69f3d4
przemekwrona <przemekwrona25@gmail.com>
2022-12-04 13:58:26 +0100

    feat: collect timetables

--- iot-gps-collector/src/main/java/pl/wrona/iot/gps/collector/sink/GCloudFileNameProvider.java
+++ iot-gps-collector/src/main/java/pl/wrona/iot/gps/collector/sink/GCloudFileNameProvider.java
@@ -4,7 +4,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import pl.wrona.iot.gps.collector.config.GCloudProperties;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 
 @Component
 @AllArgsConstructor
@@ -12,8 +14,12 @@ public class GCloudFileNameProvider {
 
     private final GCloudProperties gCloudProperties;
 
-    public String vehiclesLive(LocalDate date) {
-        String fileName = String.format("%s_%s.parquet", gCloudProperties.getBucketName(), date);
-        return String.format("gs://%s/%s", gCloudProperties.getBucketName(), fileName);
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = new DateTimeFormatterBuilder()
+            .appendPattern("yyyy-MM-dd--HH")
+            .toFormatter();
+
+    public String vehiclesLive(LocalDateTime date) {
+        String fileName = String.format("%s_%s.parquet", gCloudProperties.getBucketName(), date.format(DATE_TIME_FORMATTER));
+        return String.format("gs://%s/date=%s/%s", gCloudProperties.getBucketName(), date.toLocalDate(), fileName);
     }
 }
---
+++ iot-gps-collector/src/test/groovy/pl/wrona/iot/gps/collector/IotGpsCollectorApplicationTest.groovy
@@ -0,0 +1,12 @@
+package pl.wrona.iot.gps.collector
+
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class IotGpsCollectorApplicationTest extends Specification {
+
+    def "should load context"() {
+
+    }
+}
