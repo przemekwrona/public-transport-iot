1239a9519648e9715fae8304af78c5f9d9e8d8d6
przemekwrona <przemekwrona25@gmail.com>
2022-12-05 15:11:10 +0100

    feat: run job to get Timetables Async

--- iot-gps-collector/src/main/java/pl/wrona/iot/gps/collector/job/WarsawTimetablesJob.java
+++ iot-gps-collector/src/main/java/pl/wrona/iot/gps/collector/job/WarsawTimetablesJob.java
@@ -1,13 +1,19 @@
 package pl.wrona.iot.gps.collector.job;
 
 import lombok.AllArgsConstructor;
+import org.apache.hadoop.fs.Path;
 import org.springframework.stereotype.Component;
 import pl.wrona.iot.gps.collector.client.WarsawStopService;
 import pl.wrona.iot.gps.collector.client.WarsawTimetableService;
+import pl.wrona.iot.gps.collector.config.GCloudProperties;
+import pl.wrona.iot.gps.collector.sink.GCloudFileNameProvider;
+import pl.wrona.iot.gps.collector.sink.GCloudSink;
+import pl.wrona.iot.gps.collector.sink.WarsawTimetableGenericRecordMapper;
 import pl.wrona.iot.gps.collector.timetable.Timetable;
 import pl.wrona.warsaw.transport.api.model.WarsawStopValue;
 import pl.wrona.warsaw.transport.api.model.WarsawTimetableValue;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -19,36 +25,61 @@ public class WarsawTimetablesJob implements Runnable {
     private final WarsawStopService warsawStopService;
     private final WarsawTimetableService warsawTimetableService;
 
+    private final GCloudFileNameProvider gCloudFileNameProvider;
+
+    private GCloudProperties gCloudProperties;
+
     @Override
     public void run() {
         LocalDate now = LocalDate.now();
 
-        warsawStopService.getStops().getResult()
-                .forEach(stop -> {
-                    String stopId = stop.getValues().stream().filter(v -> v.getKey().equals("zespol")).findFirst().map(WarsawStopValue::getValue).orElse("");
-                    String stopNumber = stop.getValues().stream().filter(v -> v.getKey().equals("slupek")).findFirst().map(WarsawStopValue::getValue).orElse("");
+        try (GCloudSink<Timetable> gCloudSink = new GCloudSink<>(new WarsawTimetableGenericRecordMapper(gCloudProperties.warsawTimetableBucket().getSchema()), new Path(gCloudFileNameProvider.vehiclesTimetables(now)), gCloudProperties)) {
+            warsawStopService.getStops().getResult()
+                    .forEach(stop -> {
+                        String stopId = stop.getValues().stream().filter(v -> v.getKey().equals("zespol")).findFirst().map(WarsawStopValue::getValue).orElse("");
+                        String stopNumber = stop.getValues().stream().filter(v -> v.getKey().equals("slupek")).findFirst().map(WarsawStopValue::getValue).orElse("");
 
-                    warsawStopService.getLinesOnStop(stopId, stopNumber).getResult().forEach(lineOnStop -> {
-                        String line = lineOnStop.getValues().stream().filter(v -> v.getKey().equals("linia")).findFirst().map(WarsawTimetableValue::getValue).orElse("");
+                        warsawStopService.getLinesOnStop(stopId, stopNumber).getResult().forEach(lineOnStop -> {
+                            String line = lineOnStop.getValues().stream().filter(v -> v.getKey().equals("linia")).findFirst().map(WarsawTimetableValue::getValue).orElse("");
 
-                        warsawTimetableService.getTimetable(stopId, stopNumber, line).getResult().forEach(timetable -> {
-                            LocalTime time = timetable.getValues().stream().filter(v -> v.getKey().equals("czas")).findFirst().map(WarsawTimetableValue::getValue).map(LocalTime::parse).orElse(null);
-                            String brigade = timetable.getValues().stream().filter(v -> v.getKey().equals("brygada")).findFirst().map(WarsawTimetableValue::getValue).orElse("");
-                            String direction = timetable.getValues().stream().filter(v -> v.getKey().equals("kierunek")).findFirst().map(WarsawTimetableValue::getValue).orElse("");
-                            String route = timetable.getValues().stream().filter(v -> v.getKey().equals("trasa")).findFirst().map(WarsawTimetableValue::getValue).orElse("");
+                            warsawTimetableService.getTimetable(stopId, stopNumber, line).getResult().forEach(timetable -> {
+                                LocalDateTime dateTime = timetable.getValues().stream()
+                                        .filter(v -> v.getKey().equals("czas"))
+                                        .findFirst()
+                                        .map(WarsawTimetableValue::getValue)
+                                        .map(v -> v.split(":"))
+                                        .map(array -> {
+                                            if (Integer.parseInt(array[0]) >= 24) {
+                                                return LocalDateTime.of(now.plusDays(1), LocalTime.of(Integer.parseInt(array[0]) - 24, Integer.parseInt(array[1]), Integer.parseInt(array[2])));
+                                            } else {
+                                                return LocalDateTime.of(now, LocalTime.of(Integer.parseInt(array[0]), Integer.parseInt(array[1]), Integer.parseInt(array[2])));
+                                            }
+                                        })
+                                        .orElseThrow();
 
-                            Timetable record = Timetable.builder()
-                                    .stopId(stopId)
-                                    .stopNumber(stopNumber)
-                                    .line(line)
-                                    .brigade(brigade)
-                                    .direction(direction)
-                                    .time(LocalDateTime.of(now, time))
-                                    .build();
+                                String brigade = timetable.getValues().stream().filter(v -> v.getKey().equals("brygada")).findFirst().map(WarsawTimetableValue::getValue).orElse("");
+                                String direction = timetable.getValues().stream().filter(v -> v.getKey().equals("kierunek")).findFirst().map(WarsawTimetableValue::getValue).orElse("");
+                                String route = timetable.getValues().stream().filter(v -> v.getKey().equals("trasa")).findFirst().map(WarsawTimetableValue::getValue).orElse("");
 
+                                Timetable record = Timetable.builder()
+                                        .stopId(stopId)
+                                        .stopNumber(stopNumber)
+                                        .line(line)
+                                        .brigade(brigade)
+                                        .direction(direction)
+                                        .time(dateTime)
+                                        .build();
+
+                                gCloudSink.save(record);
+
+                            });
                         });
                     });
-                });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- iot-gps-collector/src/main/java/pl/wrona/iot/gps/collector/scheduler/WarsawTimetableScheduler.java
+++ iot-gps-collector/src/main/java/pl/wrona/iot/gps/collector/scheduler/WarsawTimetableScheduler.java
@@ -1,17 +1,22 @@
 package pl.wrona.iot.gps.collector.scheduler;
 
 import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import pl.wrona.iot.gps.collector.job.WarsawTimetablesJob;
 
+
+@EnableAsync
 @Component
 @AllArgsConstructor
 public class WarsawTimetableScheduler implements Runnable {
 
     private final WarsawTimetablesJob warsawTimetablesJob;
 
-    @Scheduled(fixedRate = 5_000)
+    @Async
+    @Scheduled(cron = "0 0 6 * * ?")
     @Override
     public void run() {
         warsawTimetablesJob.run();
