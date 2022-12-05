307cff80e3cd5504b9669fb500fb24df6b4daed3
przemekwrona <przemekwrona25@gmail.com>
2022-12-05 14:05:06 +0100

    feat: define file name definition

--- iot-gps-collector/src/main/java/pl/wrona/iot/gps/collector/sink/GCloudFileNameProvider.java
+++ iot-gps-collector/src/main/java/pl/wrona/iot/gps/collector/sink/GCloudFileNameProvider.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import pl.wrona.iot.gps.collector.config.GCloudProperties;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
@@ -15,11 +16,20 @@ public class GCloudFileNameProvider {
     private final GCloudProperties gCloudProperties;
 
     private static final DateTimeFormatter DATE_TIME_FORMATTER = new DateTimeFormatterBuilder()
-            .appendPattern("yyyy-MM-dd--HH")
+            .appendPattern("yyyy_MM_dd__HH")
+            .toFormatter();
+
+    private static final DateTimeFormatter DATE_FORMATTER = new DateTimeFormatterBuilder()
+            .appendPattern("yyyy_MM_dd")
             .toFormatter();
 
     public String vehiclesLive(LocalDateTime date) {
-        String fileName = String.format("%s_%s.parquet", gCloudProperties.getBucketName(), date.format(DATE_TIME_FORMATTER));
-        return String.format("gs://%s/date=%s/%s", gCloudProperties.getBucketName(), date.toLocalDate(), fileName);
+        String fileName = String.format("%s_%s.parquet", gCloudProperties.warsawVehicleLiveBucket().getBucketName(), date.format(DATE_TIME_FORMATTER));
+        return String.format("gs://%s/%s/%s", gCloudProperties.warsawVehicleLiveBucket().getBucketName(), date.format(DATE_FORMATTER), fileName);
+    }
+
+    public String vehiclesTimetables(LocalDate date) {
+        String fileName = String.format("%s_%s.parquet", gCloudProperties.warsawTimetableBucket().getBucketName(), date.format(DATE_TIME_FORMATTER));
+        return String.format("gs://%s/%s/%s", gCloudProperties.warsawTimetableBucket().getBucketName(), date.format(DATE_FORMATTER), fileName);
     }
 }
--- iot-gps-collector/src/test/groovy/pl/wrona/iot/gps/collector/sink/GCloudFileNameProviderTest.groovy
+++ iot-gps-collector/src/test/groovy/pl/wrona/iot/gps/collector/sink/GCloudFileNameProviderTest.groovy
@@ -13,14 +13,15 @@ class GCloudFileNameProviderTest extends Specification {
 
     def "should return file name"() {
         given:
-        gCloudProperties.getBucketName() >> "vehicles_live"
+        gCloudProperties.getBucketName() >> "warsaw_vehicles_live"
 
         expect:
         gCloudFileNameProvider.vehiclesLive(DATE_TIME) == PATH
 
         where:
         DATE_TIME                              || PATH
-        LocalDateTime.of(2022, 12, 13, 14, 15) || "gs://vehicles_live/date=2022-12-13/vehicles_live_2022-12-13--14.parquet"
+        LocalDateTime.of(2022, 12, 13, 14, 15) || "gs://warsaw_vehicles_live/2022_12_13/warsaw_vehicles_live_2022_12_13__14.parquet"
+        LocalDateTime.of(2022, 12, 13, 6, 15)  || "gs://warsaw_vehicles_live/2022_12_13/warsaw_vehicles_live_2022_12_13__06.parquet"
 
     }
 }
