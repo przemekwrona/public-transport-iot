038d6bcee92512c6560b6619ddc7e74508d0c401
pwrona <pwrona@netguardians.ch>
2023-08-19 12:22:06 +0200

    generate warsaw gtfs

--- iot-timetable/src/main/java/pl/wrona/iot/timetable/reload/ReloadService.java
+++ iot-timetable/src/main/java/pl/wrona/iot/timetable/reload/ReloadService.java
@@ -2,8 +2,15 @@ package pl.wrona.iot.timetable.reload;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.parquet.avro.AvroParquetReader;
+import org.apache.parquet.hadoop.ParquetReader;
+import org.apache.parquet.hadoop.util.HadoopInputFile;
+import org.onebusaway.gtfs.model.*;
+import org.onebusaway.gtfs.serialization.GtfsWriter;
 import org.springframework.stereotype.Service;
 import pl.wrona.iot.timetable.client.warsaw.WarsawApiService;
 import pl.wrona.iot.timetable.client.warsaw.WarsawDepartures;
@@ -12,16 +19,20 @@ import pl.wrona.iot.timetable.client.warsaw.WarsawStop;
 import pl.wrona.iot.timetable.client.warsaw.WarsawStopService;
 import pl.wrona.iot.timetable.properties.IotTimetablesProperties;
 import pl.wrona.iot.timetable.sink.ParquetSink;
+import pl.wrona.iot.warsaw.avro.WarsawPositions;
 import pl.wrona.iot.warsaw.avro.WarsawTimetable;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+
+import static java.util.Objects.checkFromIndexSize;
+import static java.util.Objects.nonNull;
 
 @Slf4j
 @Service
@@ -92,5 +103,94 @@ public class ReloadService {
                 .flatMap(Collection::stream)
                 .map(stop -> Pair.of(stop, warsawApiService.getTimetable(stop.getStopId(), stop.getStopNumber(), stop.getLines().get(0))))
                 .collect(Collectors.toList());
+    }
+
+    public void gtfs(LocalDate date) {
+        java.nio.file.Path path = java.nio.file.Path.of(iotTimetablesProperties.getDirPath(), String.format("%s/%s.warsaw.timetable.parquet", date, date));
+        try (ParquetReader<WarsawTimetable> reader = AvroParquetReader
+                .<WarsawTimetable>builder(HadoopInputFile.fromPath(new Path(path.toUri()), new Configuration()))
+                .build()) {
+
+            List<WarsawTimetable> timetables = new ArrayList<>();
+
+            WarsawTimetable record = reader.read();
+            while (nonNull(record)) {
+                timetables.add(record);
+                record = reader.read();
+            }
+
+            GtfsWriter writer = new GtfsWriter();
+            writer.setOutputLocation(new File(iotTimetablesProperties.getDirPath()));
+
+            Agency agency = new Agency();
+            agency.setId("ZTM Warszawa");
+            agency.setName("Warszawski Transport Publiczny");
+            agency.setUrl("https://www.wtp.waw.pl/");
+            agency.setTimezone("Europe/Warsaw");
+
+            writer.handleEntity(agency);
+
+            timetables.stream()
+                    .map(WarsawStop::of)
+                    .collect(Collectors.toSet()).stream()
+                    .sorted(Comparator.comparing(WarsawStop::getGroup))
+                    .map(warsawStop -> {
+                        AgencyAndId stopId = new AgencyAndId();
+                        stopId.setId(String.format("%s_%s", warsawStop.getGroup(), warsawStop.getSlupek()));
+
+                        Stop stop = new Stop();
+                        stop.setId(stopId);
+                        stop.setDirection(warsawStop.getDirection());
+                        stop.setName(String.format("%s %s", warsawStop.getName(), warsawStop.getSlupek()));
+                        stop.setLat(warsawStop.getLat());
+                        stop.setLon(warsawStop.getLon());
+                        return stop;
+                    }).forEach(writer::handleEntity);
+
+            Map<String, Map<String, Long>> linesAndTheirDirection = timetables.stream()
+                    .collect(Collectors.groupingBy(v -> v.getLine().toString(),
+                            Collectors.groupingBy(f -> f.getLineDirection().toString(), Collectors.counting())));
+
+            linesAndTheirDirection.keySet().stream().sorted().map(line -> {
+                List<Pair<String, Long>> directions = linesAndTheirDirection
+                        .get(line)
+                        .keySet().stream()
+                        .map(key -> Pair.of(key, linesAndTheirDirection.get(line).get(key)))
+                        .sorted(Comparator.comparing(Pair::getRight))
+                        .collect(Collectors.toList());
+
+                AgencyAndId agencyAndId = new AgencyAndId();
+                agencyAndId.setId(line);
+
+                String first = directions.get(directions.size() - 2).getKey();
+                String last = directions.get(directions.size() - 1).getKey();
+
+                Route route = new Route();
+                route.setId(agencyAndId);
+                route.setAgency(agency);
+                route.setShortName(line);
+                route.setLongName(String.format("%s - %s", first, last));
+
+                return route;
+            }).forEach(writer::handleEntity);
+
+            //
+//            timetables.stream()
+//                    .map(WarsawTimetable::getLine)
+//                    .collect(Collectors.toSet()).stream()
+//                    .map(line -> {
+//                        List<WarsawTimetable> aa = timetables.stream()
+//                                .filter(a -> a.getLine().toString().contentEquals(line))
+//                                .collect(Collectors.toList());
+//
+//                        return "";
+//                    }).collect(Collectors.toList());
+
+
+            writer.close();
+
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 }
