9ffc0c78a351ee364d007247ae3ef6441374c811
przemekwrona <przemekwrona25@gmail.com>
2023-08-20 12:11:57 +0200

    feat: build gtfs file

--- iot-timetable/src/main/java/pl/wrona/iot/timetable/reload/ReloadService.java
+++ iot-timetable/src/main/java/pl/wrona/iot/timetable/reload/ReloadService.java
@@ -2,14 +2,12 @@ package pl.wrona.iot.timetable.reload;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.parquet.avro.AvroParquetReader;
 import org.apache.parquet.hadoop.ParquetReader;
 import org.apache.parquet.hadoop.util.HadoopInputFile;
-import org.onebusaway.gtfs.model.*;
 import org.onebusaway.gtfs.serialization.GtfsWriter;
 import org.springframework.stereotype.Service;
 import pl.wrona.iot.timetable.client.warsaw.WarsawApiService;
@@ -19,19 +17,16 @@ import pl.wrona.iot.timetable.client.warsaw.WarsawStop;
 import pl.wrona.iot.timetable.client.warsaw.WarsawStopService;
 import pl.wrona.iot.timetable.properties.IotTimetablesProperties;
 import pl.wrona.iot.timetable.sink.ParquetSink;
-import pl.wrona.iot.warsaw.avro.WarsawPositions;
 import pl.wrona.iot.warsaw.avro.WarsawTimetable;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-import static java.util.Objects.checkFromIndexSize;
 import static java.util.Objects.nonNull;
 
 @Slf4j
@@ -67,10 +62,7 @@ public class ReloadService {
     }
 
     private List<WarsawTimetable> process(WarsawStop warsawStop) {
-        return reload(warsawStop).stream()
-                .flatMap(pair -> pair.getRight().stream())
-                .map(timetable -> build(warsawStop, timetable))
-                .collect(Collectors.toList());
+        return reload(warsawStop).stream().flatMap(pair -> pair.getRight().stream()).map(timetable -> build(warsawStop, timetable)).collect(Collectors.toList());
     }
 
     private WarsawTimetable build(WarsawStop warsawStop, WarsawDepartures timetable) {
@@ -90,26 +82,12 @@ public class ReloadService {
     }
 
     private List<Pair<WarsawLineOnStop, List<WarsawDepartures>>> reload(WarsawStop warsawStop) {
-        return Optional.of(warsawStop)
-                .map(stop -> warsawStopService.getLinesOnStop(stop.getGroup(), stop.getSlupek()))
-                .map(linesOnStop -> linesOnStop.getLines().stream()
-                        .map(stop -> WarsawLineOnStop.builder()
-                                .stopId(linesOnStop.getStopId())
-                                .stopNumber(linesOnStop.getStopNumber())
-                                .lines(List.of(stop))
-                                .build())
-                        .collect(Collectors.toList()))
-                .stream()
-                .flatMap(Collection::stream)
-                .map(stop -> Pair.of(stop, warsawApiService.getTimetable(stop.getStopId(), stop.getStopNumber(), stop.getLines().get(0))))
-                .collect(Collectors.toList());
+        return Optional.of(warsawStop).map(stop -> warsawStopService.getLinesOnStop(stop.getGroup(), stop.getSlupek())).map(linesOnStop -> linesOnStop.getLines().stream().map(stop -> WarsawLineOnStop.builder().stopId(linesOnStop.getStopId()).stopNumber(linesOnStop.getStopNumber()).lines(List.of(stop)).build()).collect(Collectors.toList())).stream().flatMap(Collection::stream).map(stop -> Pair.of(stop, warsawApiService.getTimetable(stop.getStopId(), stop.getStopNumber(), stop.getLines().get(0)))).collect(Collectors.toList());
     }
 
     public void gtfs(LocalDate date) {
         java.nio.file.Path path = java.nio.file.Path.of(iotTimetablesProperties.getDirPath(), String.format("%s/%s.warsaw.timetable.parquet", date, date));
-        try (ParquetReader<WarsawTimetable> reader = AvroParquetReader
-                .<WarsawTimetable>builder(HadoopInputFile.fromPath(new Path(path.toUri()), new Configuration()))
-                .build()) {
+        try (ParquetReader<WarsawTimetable> reader = AvroParquetReader.<WarsawTimetable>builder(HadoopInputFile.fromPath(new Path(path.toUri()), new Configuration())).build()) {
 
             List<WarsawTimetable> timetables = new ArrayList<>();
 
@@ -120,74 +98,12 @@ public class ReloadService {
             }
 
             GtfsWriter writer = new GtfsWriter();
-            writer.setOutputLocation(new File(iotTimetablesProperties.getDirPath()));
+            java.nio.file.Path gtfsPath = java.nio.file.Path.of(iotTimetablesProperties.getDirPath(), date.toString(), String.format("%s.warsaw.gtfs", date));
+            writer.setOutputLocation(new File(gtfsPath.toAbsolutePath().toString()));
 
-            Agency agency = new Agency();
-            agency.setId("ZTM Warszawa");
-            agency.setName("Warszawski Transport Publiczny");
-            agency.setUrl("https://www.wtp.waw.pl/");
-            agency.setTimezone("Europe/Warsaw");
-
-            writer.handleEntity(agency);
-
-            timetables.stream()
-                    .map(WarsawStop::of)
-                    .collect(Collectors.toSet()).stream()
-                    .sorted(Comparator.comparing(WarsawStop::getGroup))
-                    .map(warsawStop -> {
-                        AgencyAndId stopId = new AgencyAndId();
-                        stopId.setId(String.format("%s_%s", warsawStop.getGroup(), warsawStop.getSlupek()));
-
-                        Stop stop = new Stop();
-                        stop.setId(stopId);
-                        stop.setDirection(warsawStop.getDirection());
-                        stop.setName(String.format("%s %s", warsawStop.getName(), warsawStop.getSlupek()));
-                        stop.setLat(warsawStop.getLat());
-                        stop.setLon(warsawStop.getLon());
-                        return stop;
-                    }).forEach(writer::handleEntity);
-
-            Map<String, Map<String, Long>> linesAndTheirDirection = timetables.stream()
-                    .collect(Collectors.groupingBy(v -> v.getLine().toString(),
-                            Collectors.groupingBy(f -> f.getLineDirection().toString(), Collectors.counting())));
-
-            linesAndTheirDirection.keySet().stream().sorted().map(line -> {
-                List<Pair<String, Long>> directions = linesAndTheirDirection
-                        .get(line)
-                        .keySet().stream()
-                        .map(key -> Pair.of(key, linesAndTheirDirection.get(line).get(key)))
-                        .sorted(Comparator.comparing(Pair::getRight))
-                        .collect(Collectors.toList());
-
-                AgencyAndId agencyAndId = new AgencyAndId();
-                agencyAndId.setId(line);
-
-                String first = directions.get(directions.size() - 2).getKey();
-                String last = directions.get(directions.size() - 1).getKey();
-
-                Route route = new Route();
-                route.setId(agencyAndId);
-                route.setAgency(agency);
-                route.setShortName(line);
-                route.setLongName(String.format("%s - %s", first, last));
-
-                return route;
-            }).forEach(writer::handleEntity);
-
-            //
-//            timetables.stream()
-//                    .map(WarsawTimetable::getLine)
-//                    .collect(Collectors.toSet()).stream()
-//                    .map(line -> {
-//                        List<WarsawTimetable> aa = timetables.stream()
-//                                .filter(a -> a.getLine().toString().contentEquals(line))
-//                                .collect(Collectors.toList());
-//
-//                        return "";
-//                    }).collect(Collectors.toList());
-
-
-            writer.close();
+            try (final GtfsGenerator gtfsGenerator = new GtfsGenerator(writer, date)) {
+                gtfsGenerator.accept(timetables);
+            }
 
         } catch (IOException exception) {
             exception.printStackTrace();
