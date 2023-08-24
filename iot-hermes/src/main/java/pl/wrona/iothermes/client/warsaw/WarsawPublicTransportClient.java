4027e9a83d25c1eeb5ecec19510a644ad9a33fa4
przemekwrona <50826441+przemekwrona@users.noreply.github.com>
2023-08-24 20:59:27 +0200

    feat: remove hermes module (#100)

--- iot-hermes/src/main/java/pl/wrona/iothermes/client/warsaw/WarsawPublicTransportClient.java
+++
@@ -1,8 +0,0 @@
-package pl.wrona.iothermes.client.warsaw;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import pl.wrona.warsaw.transport.api.ApiApi;
-
-@FeignClient(name = "${warsaw.um.api.name}", url = "${warsaw.um.api.url}")
-public interface WarsawPublicTransportClient extends ApiApi {
-}
