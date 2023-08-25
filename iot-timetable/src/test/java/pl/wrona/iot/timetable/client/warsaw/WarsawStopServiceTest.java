f083165dce2f2e52b8bf4e766b38f8d4cdddf491
przemekwrona <50826441+przemekwrona@users.noreply.github.com>
2023-08-25 18:07:21 +0200

    add junit to iot-timetable (#104)
    Co-authored-by: pwrona <pwrona@netguardians.ch>

---
+++ iot-timetable/src/test/java/pl/wrona/iot/timetable/client/warsaw/WarsawStopServiceTest.java
@@ -0,0 +1,72 @@
+package pl.wrona.iot.timetable.client.warsaw;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cache.CacheManager;
+import org.springframework.http.ResponseEntity;
+import pl.wrona.warsaw.transport.api.model.WarsawStops;
+
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static pl.wrona.iot.timetable.JsonFileUtils.readJson;
+
+@SpringBootTest
+class WarsawStopServiceTest {
+
+    @MockBean
+    private WarsawApiClient warsawApiClient;
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    @Autowired
+    private WarsawStopService warsawStopService;
+
+
+    @Test
+    void shouldCallWarsawAPI_1_time() {
+        // given
+        when(warsawApiClient.getStops(any(), any())).thenReturn(ResponseEntity.ok(new pl.wrona.warsaw.transport.api.model.WarsawStops().result(List.of())));
+
+        // when
+        IntStream.of(10)
+                .forEach(i -> warsawStopService.getWarsawStop("1001", "01"));
+
+        // then
+        verify(warsawApiClient, times(1)).getStops(any(), any());
+    }
+
+    private static Stream<Arguments> provideStops() {
+        return Stream.of(
+                Arguments.of(52.214f, 20.980f, "Och-Teatr"),
+                Arguments.of(52.220f, 20.985f, "pl.Narutowicza"),
+                Arguments.of(52.222f, 20.985f, "Ochota-Ratusz"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideStops")
+    void shouldReturnTheClosestStop(Float lat, Float lon, String name) throws IOException {
+        WarsawStops warsawStops = readJson("/warsaw/stops-narutowicza.json", WarsawStops.class);
+        when(warsawApiClient.getStops(any(), any())).thenReturn(ResponseEntity.ok(warsawStops));
+
+        // when
+        WarsawStop warsawStop = warsawStopService.getStopsInAreaOf35m(lat, lon);
+
+        // then
+        assertThat(warsawStop).isNotNull();
+        assertThat(warsawStop.getName()).isEqualTo(name);
+    }
+
+}
